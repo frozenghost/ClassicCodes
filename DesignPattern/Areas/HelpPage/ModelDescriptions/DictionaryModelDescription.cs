b78fb0169d0c47c2a383b8d02c2b6a91e25d66d9
tim.huang <hc092783@gmail.com>
2018-11-27 14:51:31 +0800

    添加项目文件。

---
+++ DesignPattern/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DesignPattern.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DesignPattern/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DesignPattern.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
